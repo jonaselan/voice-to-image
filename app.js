64b1a0f40115dcd175336d2ac4efe8c45adeb4f8
Jonas Elan <jonas.elan@gmail.com>
2020-05-23 13:04:40 -0300

    Fix song lyrics fetch

--- app.js
+++ app.js
@@ -7,38 +7,45 @@ const app = express() // Create Express app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', __dirname)
 
-const axios_default = require('axios').default;
-const axios = axios_default.create({
+const axios = require('axios').default;
+const axios_genius = axios.create({
   baseURL: process.env.GENIUS_API_URL,
   headers: { 'Authorization': `Bearer ${process.env.GENIUS_TOKEN}` }
 });
 
-// TODO: split in multiple files: api, pages, routes
-
-async function fetchLyric(song_id) {
-  request({ uri: `https://genius.com/songs/${song_id}`, }, function(error, response, body) {
-    var $ = cheerio.load(body);
-    var text
-
-    $(".lyrics").each(function() {
-      var link = $(this);
-      text = link.text();
-
-      // console.log(text);
-    });
-
-    return text
-  });
-}
-
 async function search(term) {
-  return axios.get(`/search?q=${term}`).then(function (response) {
+  return axios_genius.get(`/search?q=${term.trim()}`).then(function (response) {
     if (response.data.meta.status == 200) {
       return response.data.response.hits;
     }
   }).catch(function (error) {
     console.log(error);
   })
+}
+
+async function getSongWebPage(song_id) {
+  return axios.get(`https://genius.com/songs/${song_id}`).then(function (response) {
+    return response.data;
+  }).catch(function (error) {
+    console.log(error);
+  })
+}
+
+async function fetchLyric(song_id) {
+  return Promise.resolve(getSongWebPage(song_id)).then(rawBody => {
+    var $ = cheerio.load(rawBody, { normalizeWhitespace: true });
+    var lyric = ''
+
+    $(".lyrics").each(function() {
+      var link = $(this);
+      lyric = link.text();
+    });
+
+    return lyric
+
+  }).catch(err => {
+    console.log(err)
+  });
 }
 
 app.get('/generate/:song_id', async function(req, res){
@@ -48,8 +55,7 @@ app.get('/generate/:song_id', async function(req, res){
   // TODO: ignore '.', ','
 
   const { song_id } = req.params
-  const lyric = fetchLyric(song_id)
-  console.log(lyric);
+  const lyric = await fetchLyric(song_id)
 
   // TODO: retornar para a tela atual
   // res.render('index.ejs')
