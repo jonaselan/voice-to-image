781028cbfaebf7ca33fabd4eb2b876337831d3f5
Jonas Elan <jonas.elan@gmail.com>
2020-06-06 08:23:30 -0300

    Add speech to text feature
    plus add initial style

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
-const express = require('express')
 const bodyParser = require('body-parser')
 const chalk = require('chalk');
+const express = require('express')
 const app = express() // Create Express app
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(require('./routes'));
 app.set('views', __dirname)
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, () => console.log(chalk.green('Server running on port 3000!')))
